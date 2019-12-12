30629761f3af0e9761e94a3b27614f04574911d9
Heedo Kim <devheedoo@gmail.com>
2019-12-12 14:18:33 +0900

    Add TypeScript

--- src/components/App.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-const App = (props) => {
-  return ( <div>Hi, I'm Choco!</div> );
-};
-export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -3,20 +3,32 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: ['@babel/polyfill', './src/index.js'],
+  entry: ['@babel/polyfill', './src/index.tsx'],
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.[hash].js'
   },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+  },
   module: {
     rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        include: [
+          path.resolve(__dirname, 'src'),
+        ],
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
       {
         test: /\.(js|jsx)$/,
         include: [
           path.resolve(__dirname, 'src'),
         ],
         exclude: /node_modules/,
-        use: 'babel-loader',
+        use: 'source-map-loader',
+        enforce: 'pre'
       },
     ]
   },
